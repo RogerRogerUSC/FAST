7c96bfd0095fd3d390acf037a0650fb343f5732e
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2023-11-30 17:52:13 -0800

    Add figure saving

--- dirichlet_plot.py
+++ dirichlet_plot.py
@@ -4,12 +4,13 @@
                  with different parameters. 
 """
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-data_dirichlet_alpha = 0.01
+data_dirichlet_alpha = 1
 num_classes = 10
-num_clients = 50
+num_clients = 30
 data_dist = np.random.dirichlet(
     data_dirichlet_alpha * np.ones(num_classes), num_clients
 )
@@ -18,8 +19,14 @@ left = 0
 for i in range(num_classes):
     plt.barh(range(num_clients), data_dist.transpose()[i], left=left, color=f"C{i}")
     left += data_dist.transpose()[i]
-plt.title("alpha=" + str(data_dirichlet_alpha))
-plt.ylabel("Clients", fontsize=12)
+plt.title("Alpha=" + str(data_dirichlet_alpha),fontsize=14)
+plt.xlabel("Proportion", fontsize=14)
+plt.ylabel("Client index", fontsize=14)
+plt.savefig(
+    os.path.join(
+        "figures", "dirichlet_alpha_"+str(data_dirichlet_alpha)+"_clients_"+str(num_clients) + ".pdf"
+    )
+)
 plt.show()
 
 # s = np.random.dirichlet((10, 5, 3), 20).transpose()
