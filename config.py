e52ca85f0a5c1c29b85854417004d5c59af09b19
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2023-12-08 13:50:45 -0800

    Add MacOS GPU training

--- config.py
+++ config.py
@@ -1,45 +1,30 @@
 import argparse
 
+
 # Parameters
-def get_parms(dataset): 
-    parser = argparse.ArgumentParser(description="PyTorch"+str(dataset)+"trainning")
+def get_parms(dataset):
+    parser = argparse.ArgumentParser(description="PyTorch" + str(dataset) + "trainning")
+    parser.add_argument("--train-batch-size", type=int, default=64)
+    parser.add_argument("--test-batch-size", type=int, default=1000)
+    parser.add_argument("--lr", type=float, default=0.01, help="Learning rate")
+    parser.add_argument("--sampling-type", type=str, default="uniform")
+    parser.add_argument("--local-update", type=int, default=10, help="Local iterations")
+    parser.add_argument("--num-clients", type=int, default=100, help="Total clients")
+    parser.add_argument("--round", type=int, default=500, help="Communication rounds")
+    parser.add_argument("--q", type=float, default=1, help="Probability")
+    parser.add_argument("--alpha", type=float, default=0.1, help="Dirichlet parameter")
     parser.add_argument(
-        "--batch-size",
-        type=int,
-        default=64,
-        metavar="N",
-        help="input batch size for training (default: 64)",
-    )
-    parser.add_argument(
-        "--test-batch-size",
-        type=int,
-        default=256,
-        metavar="N",
-        help="input batch size for testing (default: 1000)",
-    )
-    parser.add_argument(
-        "--epoch",
-        type=int,
-        default=10000,
-        metavar="N",
-        help="number of epochs to train (default: 10)",
-    )
-    parser.add_argument(
-        "--lr", type=float, default=0.01, metavar="LR", help="learning rate (default: 0.01)"
+        "--data-dist", type=str, default="dirichlet", help="[uniform, dirichlet]"
     )
     parser.add_argument(
         "--no-cuda", action="store_true", default=False, help="disables CUDA training"
     )
-    parser.add_argument("--seed", type=int, default=42, help="random seed")
-    parser.add_argument("--sampling_type", type=str, default="uniform", help="")
-    parser.add_argument("--local_update", type=int, default=10, help="Local iterations")
     parser.add_argument(
-        "--num_clients", type=int, default=100, help="Total number of clients"
+        "--no-mps",
+        action="store_true",
+        default=False,
+        help="disables macOS GPU training",
     )
-    parser.add_argument("--rounds", type=int, default=10000, help="The number of rounds")
-    parser.add_argument("--q", type=float, default=0.5, help="Probability q")
-    parser.add_argument(
-        "--alpha", type=float, default=0.1, help="Dirichlet Distribution parameter"
-    )
-    parser.add_argument("--data_distribution", type=str, default="uniform", help="[uniform, dirichlet]")
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=53, help="random seed")
     return parser
