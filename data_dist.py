db5de1c577af5a92382df78bb6fa4905de6d4b28
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2023-12-07 20:08:11 -0800

    Modify DirichletSampler

--- data_dist.py
+++ data_dist.py
@@ -3,6 +3,7 @@ import math
 import torch
 import numpy as np
 from torch.utils.data import Sampler
+from sinkhorn_knopp import sinkhorn_knopp as skp
 
 
 def get_data_sampler(dataset, args, idx):
@@ -15,7 +16,11 @@ def get_data_sampler(dataset, args, idx):
     # Sample data with Dirichlet distribution
     elif args.data_distribution == "dirichlet":
         sampler = DirichletSampler(
-            dataset=dataset, size=args.num_clients, rank=idx, alpha=args.alpha
+            dataset=dataset,
+            size=args.num_clients,
+            rank=idx,
+            alpha=args.alpha,
+            shuffle=True,
         )
         return sampler
     else:
@@ -39,6 +44,7 @@ class DirichletSampler(Sampler):
 
         # Group the index of same label together
         self.indices_per_label = [[] for _ in range(len(self.labels))]
+
         for index, label in enumerate(self.dataset.targets):
             self.indices_per_label[label].append(index)
 
@@ -56,16 +62,19 @@ class DirichletSampler(Sampler):
         rng = np.random.default_rng(random_seed)
         # This is a matrix with dimension "size * num_classes"
         self.full_class_prob = rng.dirichlet(alpha * np.ones(len(self.labels)), size)
+        # To convert an non-negative square matrix with total support into a doubly stochastic matrix.
+        sk = skp.SinkhornKnopp()
+        doubly_stochastic_matrix = sk.fit(self.full_class_prob)
 
         # Prepare the dataset via the partition according to the prob.
         self.my_index = []
         for label in self.labels:
-            cum_prob = np.cumsum(self.full_class_prob[:, label])
+            cum_prob = np.cumsum(doubly_stochastic_matrix[:, label])
             normalized_cum_prob = (cum_prob / cum_prob[-1]).tolist()
             label_indices = self.indices_per_label[label]
             start_prob = 0 if rank == 0 else normalized_cum_prob[rank - 1]
             end_prob = normalized_cum_prob[rank]
-            my_index_start = math.ceil(start_prob * len(label_indices))
+            my_index_start = math.floor(start_prob * len(label_indices))
             my_index_end = math.floor(end_prob * len(label_indices))
             self.my_index.extend(label_indices[my_index_start:my_index_end])
         self.my_index = np.random.permutation(self.my_index)
