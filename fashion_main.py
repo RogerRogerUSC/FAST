d590df0d7756475e7ed891f956b742b464819c6e
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2023-12-14 19:26:53 -0800

    Successful version

--- fashion_main.py
+++ fashion_main.py
@@ -4,25 +4,37 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from agent_utils import Agent, Server
 from tqdm import tqdm
 from client_sampling import client_sampling
 from log import log
-from data_dist import get_data_sampler
-from models.cnn_mnist import CNNMnist
 from local_update import local_update_selected_clients
 from config import get_parms
+from fedlab.utils.dataset.partition import FMNISTPartitioner
+from fedlab.models.cnn import CNN_FEMNIST
 
 
 args = get_parms("Fashion-MNIST").parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
+print(args)
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+use_mps = not args.no_mps and torch.backends.mps.is_available()
+
 torch.manual_seed(args.seed)
 
+if use_cuda:
+    device = torch.device("cuda")
+    print("===cuda")
+elif use_mps:
+    device = torch.device("mps")
+    print("===mps")
+else:
+    device = torch.device("cpu")
+    print("===cpu")
 
-kwargs = {"num_workers": 1, "pin_memory": True} if args.cuda else {}
+
+kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
 current_loc = os.path.dirname(os.path.abspath(__file__))
 transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
@@ -41,21 +53,31 @@ test_dataset = datasets.FashionMNIST(
     transform=transform,
 )
 test_loader = torch.utils.data.DataLoader(
-    test_dataset, batch_size=args.test_batch_size, shuffle=False, **kwargs
+    test_dataset, batch_size=args.test_batch_size, shuffle=True, **kwargs
+)
+
+train_dataset_partition = FMNISTPartitioner(
+    targets=train_dataset.targets,
+    num_clients=args.num_clients,
+    partition="noniid-labeldir",
+    dir_alpha=args.alpha,
+    seed=args.seed,
 )
 
 # Create clients and server
 clients = []
 for idx in range(args.num_clients):
-    model = CNNMnist(args)
+    model = CNN_FEMNIST()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(
-        model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4
-    )
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-    sampler = get_data_sampler(dataset=train_dataset, args=args, idx=idx)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    # sampler = get_data_sampler(dataset=train_dataset, args=args, idx=idx)
+    # train_loader = torch.utils.data.DataLoader(
+    #     train_dataset, batch_size=args.batch_size, sampler=sampler, **kwargs
+    # )
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, sampler=sampler, **kwargs
+        dataset=torch.utils.data.Subset(train_dataset, train_dataset_partition[idx]),
+        batch_size=args.train_batch_size,
+        **kwargs,
     )
     clients.append(
         Agent(
@@ -63,17 +85,16 @@ for idx in range(args.num_clients):
             optimizer=optimizer,
             criterion=criterion,
             train_loader=train_loader,
+            device=device,
         )
     )
-server = Server(model=CNNMnist(args), criterion=criterion)
+
+server = Server(model=CNN_FEMNIST(), criterion=criterion, device=device)
 
 
 writer = SummaryWriter(
     os.path.join(
-        "output",
-        "fashion-mnist",
-        f"alpha={args.alpha}+{args.sampling_type}+q={args.q}+num_clients={args.num_clients}+lr={args.lr}",
-        # datetime.now().strftime("%m_%d-%H-%M-%S"),
+        "output", "fashion-mnist", f"{args}+{datetime.now().strftime('%m_%d-%H-%M-%S')}"
     )
 )
 
@@ -87,15 +108,18 @@ with tqdm(total=args.rounds, desc=f"Training:") as t:
             clients=sampled_clients, server=server, local_update=args.local_update
         )
         server.avg_clients(sampled_clients)
+        # Decay the learning rate every M rounds
+        # if (round+1) % 100 == 0:
+        #     [client.decay_lr_in_optimizer(gamma=0.5) for client in clients]
         writer.add_scalar("Loss/train", train_loss, round)
         writer.add_scalar("Accuracy/train", train_acc, round)
         t.set_postfix({"loss": train_loss, "accuracy": 100.0 * train_acc})
         t.update(1)
-        if round % 10 == 0:
+        if round % 5 == 0:
             eval_loss, eval_acc = server.eval(test_loader)
             writer.add_scalar("Loss/test", eval_loss, round)
             writer.add_scalar("Accuracy/test", eval_acc, round)
-            print(f"Evaluation(round {round+1}): {eval_loss=:.4f} {eval_acc=:.3f}")
+            print(f"Evaluation(round {round}): {eval_loss=:.4f} {eval_acc=:.3f}")
             log(round, eval_acc)
 
 print(
