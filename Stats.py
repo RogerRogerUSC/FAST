5e51f41f2f29d7e9eb27efb0a365e5aff5e7cba5
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2024-01-20 18:17:18 -0800

    Update stats.py

--- Stats.py
+++ Stats.py
@@ -1,34 +1,27 @@
-import numpy as np
 import os
 import pandas as pd
+from config import get_parms
 
+
+args = get_parms("Stats").parse_args()
 dataset = "fashion"
-alpha = "0.05"
-sample_type = "gamma"
+if "uniform_" in args.sampling_type:
+    sampling_type = args.sampling_type.replace("uniform_", "")
+else:
+    sampling_type = args.sampling_type
 
 
-# filenames = os.listdir("results/"+dataset+"/alpha_"+str(alpha)+"/")
-# for filename in filenames: 
-#     print(filename)
-#     df = pd.read_csv("results/"+dataset+"/alpha_"+str(alpha)+"/" + filename)
-#     array = df.iloc[len(df.index)-5:len(df.index),2]
-#     print(f"{array.mean()*100 =:.2f}")
-#     print(f"{array.std()*100 =:.1f}")
-#     print()
-
-# for filename in filenames:
-#     print(filename)
-#     df = pd.read_csv("results/"+dataset+"/alpha_"+str(alpha)+"/" + filename)
-#     array = df["Value"]
-#     print(array.max())
-#     print()
-
-filenames = os.listdir(f"results/{dataset}/{sample_type}_alpha_{alpha}/")
-for filename in filenames: 
+filenames = os.listdir(
+    f"results/{dataset}_{args.algo}/{sampling_type}_alpha_{args.alpha}/"
+)
+# filenames = os.listdir(f"results/{dataset}_fedprox/alpha_{alpha}/")
+for filename in filenames:
     print(filename)
-    df = pd.read_csv(f"results/{dataset}/{sample_type}_alpha_{alpha}/{filename}")
-    array = df.iloc[len(df.index)-5:len(df.index),2]
+    df = pd.read_csv(
+        f"results/{dataset}_{args.algo}/{sampling_type}_alpha_{args.alpha}/{filename}"
+    )
+    # df = pd.read_csv(f"results/{dataset}_fedprox/alpha_{alpha}/{filename}")
+    array = df.iloc[len(df.index) - 5 : len(df.index), 2]
     print(f"{array.mean()*100 =:.2f}")
     print(f"{array.std()*100 =:.1f}")
     print()
-
