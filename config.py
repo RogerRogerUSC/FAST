699a05156f3e17cd4994a84a21ef9606664f573b
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2024-09-09 16:08:28 -0400

    Add parameters.

--- config.py
+++ config.py
@@ -10,14 +10,31 @@ def get_parms(dataset):
     parser.add_argument("--sampling-type", type=str, default="uniform")
     parser.add_argument("--local-update", type=int, default=10, help="Local iterations")
     parser.add_argument("--num-clients", type=int, default=100, help="Total clients")
+    parser.add_argument("--round", type=int, default=1000, help="Communication rounds")
+    parser.add_argument("--q", type=float, default=1, help="Probability of snapshot")
     parser.add_argument(
-        "--rounds", type=int, default=1000, help="Communication rounds"
+        "--gamma", type=float, default=0.7, help="parameter for adaptive FAST"
     )
-    parser.add_argument("--q", type=float, default=1, help="Probability")
     parser.add_argument("--alpha", type=float, default=0.1, help="Dirichlet parameter")
     parser.add_argument(
         "--data-dist", type=str, default="dirichlet", help="[uniform, dirichlet]"
     )
+    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=365, help="random seed")
+    parser.add_argument("--algo", type=str, default="fedavg", help="[fedavg]")
+    parser.add_argument("--adaptive", type=int, default=0)
+    parser.add_argument("--num-workers", type=int, default=2)
+    parser.add_argument(
+        "--compressor", type=str, default="topk", help="[topk,randk, quan]"
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="mnist",
+        help="[mnist, fashion, cifar10, shakespeare]",
+    )
+    parser.add_argument("--log-to-tensorboard", type=str, default=None)
+    parser.add_argument("--eval-iterations", type=int, default=10)
     parser.add_argument(
         "--no-cuda", action="store_true", default=False, help="disables CUDA training"
     )
@@ -27,9 +44,4 @@ def get_parms(dataset):
         default=False,
         help="disables macOS GPU training",
     )
-    parser.add_argument("--epochs", type=int, default=100)
-    parser.add_argument("--seed", type=int, default=365, help="random seed")
-    parser.add_argument("--algo", type=str, default="fedavg", help="[fedavg]")
-    parser.add_argument("--adaptive", type=int, default=0)
-    parser.add_argument("--num-workers", type=int, default=2)
     return parser
